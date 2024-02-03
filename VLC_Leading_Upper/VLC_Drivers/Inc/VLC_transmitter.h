d1e5f0475b1a924ce862501fbcdb376742059ae1
Morrishohoho <459225516@qq.com>
2024-02-03 15:00:21 +0800

    Both PA1 and PA2 transmit

--- VLC_Leading_Upper/VLC_Drivers/Inc/VLC_transmitter.h
+++ VLC_Leading_Upper/VLC_Drivers/Inc/VLC_transmitter.h
@@ -10,25 +10,25 @@
  * Send the data through the VLC Transmitter.
  * @param data Data in chars, i.e. "abcdefg".
  */
-void VLC_transmitter_send(const char* data);
+void VLC_UART_transmitter_send(const char* data);
 
 
 /**
  * Sending square wave when no messages are sending.
  * TODO: Use Interrupt to stop the idle state.
  */
-void VLC_transmitter_idle();
+void VLC_UART_transmitter_idle();
 
 
 /**
- * Sending the data by using gpio.
+ * Sending the data by using gpio: PA1 and PA2.
  * @param data The original data without the PSN.
  */
 void VLC_Gpio_transmitter_send(const char *data);
 
 
 /**
- * Sending 1 second square wave signal.
+ * Sending 1 second square wave signal by using gpio: PA1 and PA2.
  */
 void VLC_Gpio_transmitter_idle();
 
--- VLC_Leading_Upper/VLC_Drivers/Src/VLC_transmitter.c
+++ VLC_Leading_Upper/VLC_Drivers/Src/VLC_transmitter.c
@@ -1,6 +1,8 @@
-//
-// Created by Administrator on 11/30/2023.
-//
+/**
+ * Created by Morris 11/30/2023
+ * This file is the implementation of VLC_transmitter.
+ * The MCU is GD32F450VKT6
+ */
 
 #include "../Inc/VLC_transmitter.h"
 #include "../Inc/VLC_encoder.h"
@@ -11,20 +13,38 @@
 #include "usart.h"
 #include <string.h>
 
-
+/**
+ * The GPIO device register operation macro function.
+ */
 #define BIT_ADDR(byte_offset, bit_number) (volatile unsigned long*)(0x42000000+((byte_offset)<< 5) +(bit_number << 2))
 #define GPIOA_OCTL_OFFSET ((GPIOA_BASE+0x14) - 0x40000000)
 
+/**
+ * Set the value of GPIO_A pins.
+ * n: The gpio A pin.
+ */
 #define PAout(n) *(BIT_ADDR(GPIOA_OCTL_OFFSET,n))
 
+/**
+ * Get the n bit value of the num v.
+ * v: The number v.
+ * n: The intended bit.
+ */
 #define GET_BIT_OF(v,n) (((v)&(1<<(n)))?1:0)
 
+/**
+ * PA1: The IR LED transmitter controlled by the upper board.
+ */
 #define IR1 1
 
+/**
+ * PA2: The IR LED transmitter controlled by the upper board.
+ */
+#define IR2 2   //PA2
 
 
 
-void VLC_transmitter_send(const char *data) {
+void VLC_UART_transmitter_send(const char *data) {
     int frame_counts = strlen(data) / PAYLOAD_LENGTH;
     if (strlen(data) % PAYLOAD_LENGTH != 0) {
         frame_counts++;
@@ -46,7 +66,7 @@ void VLC_transmitter_send(const char *data) {
 }
 
 
-void VLC_transmitter_idle() {
+void VLC_UART_transmitter_idle() {
     uint8_t square_wave[] = {0xAA};
     for (int i = 0; i < 1000; i++) {
         HAL_UART_Transmit(&huart4, square_wave, 1, HAL_MAX_DELAY);
@@ -61,12 +81,14 @@ void VLC_Gpio_transmitter_send(const char *data) {
     }
 
     PAout(IR1) = 0;
+    PAout(IR2) = 0;
     uint8_t tx_buf[FRAME_LENGTH * 2 + 2]; //payload after manchester encoding + 1 byte header + 1 byte tailer
     for (int i = 0; i < frame_counts; i++) {
         VLC_encode(data + i * PAYLOAD_LENGTH, i, tx_buf);
         for (int j = 0; j < FRAME_LENGTH * 2 + 2; j++) {
             for (int b = 7; b>=0; b--) {
                 PAout(IR1) = GET_BIT_OF(tx_buf[j],b);
+                PAout(IR2) = GET_BIT_OF(tx_buf[j],b);
                 VLC_udelay(VLC_BIT_DURATION);
             }
         }
@@ -77,6 +99,7 @@ void VLC_Gpio_transmitter_idle(){
     for(int i=0;i<50000;i++)
     {
         HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_1);
+        HAL_GPIO_TogglePin(GPIOA,GPIO_PIN_2);
         VLC_udelay(VLC_BIT_DURATION);
     }
 }
